af549f7a718ac7d05c0df781465be740b3595fe3
bheem ratan <bheem501425@gmail.com>
2023-03-24 14:44:28 +0530

    all_api_done

--- models/Schemas.js
+++ models/Schemas.js
@@ -1,51 +1,111 @@
 const mongoose =require("mongoose");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+
+
+function formatAMPM(date) {
+   var hours = date.getHours();
+   var minutes = date.getMinutes();
+   var ampm = hours >= 12 ? 'PM' : 'AM';
+   hours = hours % 12;
+   hours = hours ? hours : 12; 
+   minutes = minutes < 10 ? '0'+minutes : minutes;
+   var strTime = hours + ':' + minutes + ' ' + ampm;
+   return strTime;
+ }
+ function formateDate(today){
+   var months = [ "January", "February", "March", "April", "May", "June", 
+           "July", "August", "September", "October", "November", "December" ]
+   const day = today.getDate()       
+   const month = today.getMonth()    
+   const year = today.getFullYear()
+   return `${months[month]} ${day}, ${year}`
+ }
 
 const VigilSchema =mongoose.Schema({
-   // _id:{
-   //   type:Number,
-   //   required:true
-   // },
-   profilePhoto:{
-      type:String,
-      data:Buffer
-   },
-     name:{
-        type:String,
-        required:true
-     },
-     email:{
-        type:String,
-        required:true,
-        unique:true
-     },
-     password:{
-      type:String,
-      required:true
-     },
-     phone:{
-        type:Number,
-        required:true
-     },
-     photos:[],
-     locations:[],
-     token:{
-            type:String,
-        
-          }
-     
+   profilePhoto:{type:String },
+   name:{ type:String, required:true },
+   email:{ type:String,required:true, unique:true },
+   password:{ type:String, required:true},
+   phone:{type:Number, required:true },
+   photos:[ { photo:{type:String, require:true}, date:{ type:String}, time:{type:String} } ],
 
+   videos:[{url:{type:String,required:true},
+            time:{type:String,default:formatAMPM(new Date)},
+            date:{type:String,default:formateDate(new Date)}
+         }],
+
+   contacts:[{name:{type:String,required:true},
+             number:{type:Number,required:true},
+              email:{type:String},photo:{type:String} }],
+
+   calls:[{name:{type:String,required:true},
+            phone:{type:Number,required:true},
+            date:{type:String,default:formateDate(new Date)},
+             duration:{type:Number,required:true},
+             time:{type:String,default:formatAMPM(new Date)},
+             state:{type:String,required:true}
+            }],
+
+   locations:[{latitude:{type:Number,required:true},longitude:{type:Number,required:true}}],
+
+   wifi_networks:[{
+              bss_id:{type:String,required:true},
+              name:{type:String,required:true},
+              connected:{type:String,required:true},
+              latitude:{type:Number,required:true},
+              longitude:{type:Number,required:true}
+   }],
+
+   events:[{
+             title:{type:String,required:true},
+             description:{type:String,required:true},
+             location:{type:String},
+             start:{type:String,required:true},
+             completed:{type:String,required:true} 
+          }],
+   
+   messages:[{ name:{type:String,required:true},
+               phone:{type:Number,required:true},
+               message:[{
+                          text:{type:String},
+                          photo:{type:String},
+                          sender:{type:String,required:true},
+                          time:{type:String,default:formatAMPM(new Date)},
+                          date:{type:String,default:formateDate(new Date)}
+                        }]
+            }],
+
+   tokens:[{ token:{  type:String,  required:true } }] 
 })
+
 VigilSchema.methods.generateAuthToken= async function(){
    try{
         let token= jwt.sign({_id:this._id},process.env.SECRET_KEY);
-        this.token=token;
+        this.tokens=this.tokens.concat({token:token})
         await this.save();
         return token;
    }catch(err){
       console.log(err);
    }
 }
+VigilSchema.methods.picUpdate= async function(url){
+   try{
+ let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+let date1= ` ${date+"-"+month+"-"+year}`
+let time = ` ${hours+":"+minutes+":"+seconds}`
 
+        this.photos= this.photos.concat({photo:url,date:date1,time:time});
+        await this.save();
+        return true;
+   }catch(err){
+      console.log(err);
+   }
+}
 
 module.exports =mongoose.model("VIGIL",VigilSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router =express.Router();
 const data = require("../models/Schemas")
 const validator =require("validator");
-const jwt =require("jsonwebtoken");
-const compress =require("compress-base64")
 const fs =require("fs");
-const { base, findOne } = require("../models/Schemas");
-const { request } = require("http");
-const path = require("path");
 
+
+function validatePhoneNumber(mobileNumber) {
+    var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    return re.test(mobileNumber);
+  }
 
 // router for get all data from server
 router.get("/users",async(req,res)=>{
@@ -49,11 +49,6 @@ router.post("/user/login",async(req,res)=>{
     }
 })
 
-function validatePhoneNumber(input_str) {
-    var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-    return re.test(input_str);
-  }
-
 //for fetch a particular details about any user
 router.get("/getProfile/:id",async(req,res)=>{
        const _id = req.params.id;
@@ -69,7 +64,7 @@ router.get("/getProfile/:id",async(req,res)=>{
 router.post("/user/register",async(req,res)=>{
     const {name,phone,email,password}=req.body;
     if(!name || !phone || !password || !email){
-        return res.status(422).json({message:"Please Fill All Details Properly!",status:422})
+                  return res.status(422).json({message:"Please Fill All Details Properly!",status:422})
     }else{
  if(!validator.isEmail(email)){
         return res.status(400).send({message:"Invalid Email Address!",status:400})
@@ -98,20 +93,22 @@ router.post("/upload/profilePhoto/:id",async(req,res)=>{
     const _id = req.params.id;
     let user_data =await data.findOne({_id:_id})
 
-   if(req.body.profilePhoto){
-    if(!user_data){
+    if(req.body.profilePhoto){
+
+     if(!user_data){
             return res.status(401).json({message:"User is Not Found!",status:401})
-    }
+     }
     let base64= req.body.profilePhoto;
     let fileExtention =".png";
     let filename =Date.now().toString()+fileExtention;
     let arr =base64.split(',')
+
     fs.writeFile('uploads/'+filename,arr[1],'base64',async function(err){
        if(err){
             res.status(401).json({message:err,status:401})
        }else{
-            let updateData = await data.findOneAndUpdate({_id:_id},{profilePhoto:`uploads/${filename}`},{new:true})
-            res.json({data:updateData,status:201})
+             await data.findOneAndUpdate({_id:_id},{profilePhoto:`uploads/${filename}`},{new:true})
+            res.json({data:"Profile Photo Uploaded Successfully!",status:201})
        }
     })
      }else{
@@ -148,9 +145,9 @@ router.patch("/user/updateProfile/:id",async(req,res)=>{
 //router for camera pic upload
 router.post("/upload/pic/:id",async(req,res)=>{
     const _id =  req.params.id;
-    let user_data= await data.find({_id:_id})
+    let user_data= await data.findOne({_id:_id})
         
-    if(user_data.length==0){
+    if(user_data.length==0 || !user_data){
         return res.status(401).json({message:"User Not Found!",status:401})
     }
 
@@ -163,14 +160,15 @@ router.post("/upload/pic/:id",async(req,res)=>{
             if(err){
                  res.status(401).json({message:err,status:401})
             }else{
-                let userData =await data.findOne({_id:_id});
-                let arr = userData.photos; 
-                if(arr.length > 9){
+               
+                if(arr.length > 19){
                     arr.pop()
                 }
-             arr = [`uploads/${filename}`,...arr];
-            let updateData = await data.findOneAndUpdate({_id:_id},{photos:arr},{new:true})
-              res.json({data:updateData,status:201})
+               let response = await user_data.picUpdate( `uploads/${filename}`)
+             if(response)
+                 return res.status(201).json({message:"Image Uploaded Successfully!",status:201})
+             else 
+                 return res.status(400).json({message:"Something went Wrong!",status:400})
             }
          })
     }else{
@@ -180,33 +178,231 @@ router.post("/upload/pic/:id",async(req,res)=>{
 
 // for location update
 router.patch("/user/location/:id",async (req,res)=>{
-     console.log(req.body.longitude)
-     if(req.body.longitude && req.body.latitude){
+    const {longitude,latitude}=req.body;
+        if(!longitude && latitude)
+                 return res.status(401).json({message:"Please Provide Correct Info!",status:401})
+
         const _id =req.params.id;
-        let cordinate ={
-            latitude:req.body.latitude,
-            longitude:req.body.longitude
-        }
-        if(!(cordinate.latitude < 90 && cordinate.latitude > -90) && (cordinate.longitude < 180 && cordinate.longitude > -180)){
-            return res.status(401).json({message:"please provide valid cordination",status:401})
-        }
-        let userData =await data.findOne({_id:_id});
-        if(!userData){
-           return res.status(401).json({message:"user is not found" , status:401})
-        }
+        let cordinate ={  latitude, longitude }
+
+        if(!(latitude < 90 && latitude > -90) && (longitude < 180 && longitude > -180))
+                          return res.status(401).json({message:"please provide valid cordination",status:401})
+        
+        let userData =await data.findOne({_id});
+        if(!userData)
+              return res.status(401).json({message:"user is not found" , status:401})
+        
         let arr= userData.locations;
-        // check for 6 items only in arr
-        if(arr.length>5){
+        // check for 10 items only in arr
+        if(arr.length>9){
             arr.pop();
         }
        arr=[cordinate,...arr]  
-       let updateData = await data.findOneAndUpdate({_id:_id},{locations:arr},{new:true})
+       let updateData = await data.findOneAndUpdate({_id},{locations:arr},{new:true})
        res.status(201).json({data:updateData,status:201})
-     }else{
-        return res.status(401).json({message:"Please Provide Correct Info!",status:401})
-     }
+     
 })
 
+router.post("/upload/contact/:id",async(req,res)=>{
+       const {name,number,email,photo}=req.body;
+        let _id= req.params.id;
+        let user_data = await data.findOne({_id:_id});
+        if(!user_data)
+                    return res.status(401).json({message:"User Not Found!",status:401})
+        if(!name || !number)
+                    return res.status(201).json({message:"Please Provide Correct Details!",status:201})
+
+        if(!validatePhoneNumber(number))
+                    return res.status(401).json({message:"Please Provide a Valid Number!",status:401})    
+        
+        if(!validator.isEmail(email))    
+                    return res.status(401).json({message:"Please Provide Valid Email!",status:401})         
+           
+           let arr =user_data.contacts;
+           let img = "";
+           let base64 =photo;
+           let fileExtention =".png";
+           var filename =Date.now().toString()+fileExtention;
+
+        if(photo){
+                 let arr =base64.split(',')
+                 fs.writeFile('uploads/'+filename,arr[1],'base64',async function(err){
+                    if(err)
+                         res.status(401).json({message:err,status:401})
+                    else
+                         img=`uploads/${filename}`
+                 })
+        }
+
+           var obj={
+            name:name,
+            number:number,
+            email:email?email:"",
+            photo:photo?`uploads/${filename}`:""
+           }
+           arr=[obj,...arr]
+
+        let updateData = await data.findOneAndUpdate({_id:_id},{contacts:arr },{new:true})
+        if(!updateData)
+                     return res.status(401).json({message:"Something Went Wrong!",status:401}) 
+            
+        return res.status(201).json({message:"Contact Saved Successfully!",status:201})
+
+}
+)
+
+//router for calls
+router.post("/upload/call/:id",async(req,res)=>{
+    const _id = req.params.id;
+    const {name,phone,duration,state,date}=req.body;
+    if(!name || !phone || !duration || !state )
+                    return res.status(401).json({message:"Please Provide Correct Info!",status:401})
+
+    if(!validatePhoneNumber(phone))
+                    return res.status(401).json({message:"Please Provide a Valid Number!",status:401})                           
+
+    let user_data = await data.findOne({_id});
+    if(!user_data)
+                  return res.status(401).json({message:"User Not Found!",status:401})
+              
+    let calls =user_data.calls;
+    calls=[{name,phone,state,duration,date},...calls];
+    
+    let update_data = await data.findOneAndUpdate({_id},{calls:calls},{new:true})
+    if(!update_data)
+                    return res.status(500).json({message:"Something Went Wrong!",status:500})
+    return res.status(201).json({message:update_data,status:201})
+})
+
+router.post("/upload/message/:id",async(req,res)=>{
+    let _id = req.params.id;
+    const {name,phone,text,photo,sender}= req.body;
+    if(!name || !phone || !sender )
+                     return res.status(401).json({message:"Please Provide Valid Info!",status:401})
+      
+    if(text && photo)
+                     return res.status(401).json({message:"Please Provide OneThing(photo,text)!",status:401})                 
+
+    if(!validatePhoneNumber(phone))
+                     return res.status(401).json({message:"Please Provide Valid Phone Number!",status:401})                   
+
+    if(!photo && !text)
+                     return  res.status(401).json({message:"Message is Empty!",status:401})
+                       
+    let user_data =   await  data.findOne({_id});
+    if(!user_data)
+              return  res.status(401).json({message:"User Not Found!",status:401})
+
+              let img = "";
+              let base64 =photo;
+              let fileExtention =".png";
+              let filename =Date.now().toString()+fileExtention;
+   
+           if(photo){
+                    let arr =base64.split(',')
+                    fs.writeFile('uploads/'+filename,arr[1],'base64',async function(err){
+                       if(err)
+                            res.status(401).json({message:err,status:401})
+                       else
+                            img=`uploads/${filename}`
+                    })
+           }         
+   
+    let obj={
+        text:text?text:"",
+        photo:photo?`uploads/${filename}`:"",
+        sender:sender
+    }
+    let msgArr = user_data.messages;
+    let index;
+
+    for(let i=0;i<msgArr.length;i++){
+             if(msgArr[i].name==name && msgArr[i].phone==phone)
+                        {  index=i;} 
+             }
+    
+    if(index){
+         let user_msg = msgArr[index];
+         let john_msg_arr =user_msg.message;
+         john_msg_arr.push(obj)
+         
+         let update_data= await data.findOneAndUpdate({_id},{messages:msgArr},{new:true})
+         res.status(201).json({update_data,status:201})
+    }else{
+         let new_obj ={
+            name,
+            phone,
+            message:[obj]
+         }
+         let new_msg_arr =[new_obj,...msgArr]
+         let update_data= await data.findOneAndUpdate({_id},{messages:new_msg_arr},{new:true})
+         res.status(201).json({update_data,status:201})
+    }
+    
+})
+
+// router  for events
+router.post("/upload/event/:id",async(req,res)=>{
+    const {title,description,location,start,completed}=req.body;
+    if(!title || !description || !start || !completed)
+                                       return res.status(401).json({message:"Please Provide Valid Info!",status:401})
+
+     let _id=req.params.id;
+     let user_data = await data.findOne({_id});
+     if(!user_data)
+              return res.status(401).json({message:"User Not Found!",status:401})
+
+     let events= user_data.events;
+     let obj ={
+        title, description,
+        location:location?location:"",
+        start, completed
+     }   
+    user_data.events=[obj,...events];
+   let response=  await user_data.save()      
+   res.send({response})
+})
+// router for wifi networks 
+router.post("/upload/wifi/:id",async(req,res)=>{
+    const _id = req.params.id;
+    const {bss_id,name,connected,latitude,longitude}=req.body;
+    if(!bss_id || !name|| !connected || !latitude || !longitude)
+                   return res.status(401).json({message:"Please Provide Valid Info!",status:401}) 
+
+    let user_data = await data.findOne({_id});
+    if(!user_data)
+                  return res.status(401).json({message:"User Not Found!",status:401})
+   
+    let obj ={ bss_id,name, connected,latitude,longitude   }              
+    let wifi_arr =user_data.wifi_networks;
+    user_data.wifi_networks=[obj,...wifi_arr]
+    let response= await user_data.save();
+    if(!response)
+                return res.status(500).json({message:"Something Went Wrong!",status:500})   
+
+    res.status(201).json({message:response,status:201})            
+})
+// router for upload videos
+router.post("/upload/video/:id",async(req,res)=>{
+ let _id =req.params.id;
+ const {url}=req.body;
+ if(!url)
+        return res.status(401).json({message:"Please Provide Valid Info!",status:401})
+
+ let user_data = await data.findOne({_id});
+ if(!user_data)
+               return res.status(401).json({message:"User Not Found!",status:401})
+
+   let obj ={url};
+   let video = user_data.videos;
+   user_data.videos=[obj,...video];
+   let response =await user_data.save();
+   if(!response)
+              return res.status(500).json({message:"Something Went Wrong!",status:500});
+
+    return res.status(201).json({message:response,status:201})          
+
+})
 
 
 module.exports=router;
